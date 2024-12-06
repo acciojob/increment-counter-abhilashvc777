ce1e194ecc12842bcf98732a17457d314c96163e
abhilashvc777 <145086618+abhilashvc777@users.noreply.github.com>
2024-12-06 17:33:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const body = document.querySelector("body")
+const p = document.createElement("p")
+const button = document.createElement("button")
+body.appendChild(p)
+body.appendChild(button)
+let count = 0;
+p.textContent = count
+button.textContent = "Click"
+button.style.padding="10px 40px"
+button.style.fontSize="1em"
+p.style.fontSize="1.3em"
+button.addEventListener("click", ()=>{
+	p.textContent=count
+	alert(count)
+	count++
+})
